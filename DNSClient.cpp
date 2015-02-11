8345f5f570083b43bf15b8ca3f30ca8b6a6e8621
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-02-11 12:04:07 -0500

    ...

---
+++ DNSClient.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    //Initialization
+
+    while(1) {
+        //Get domain from user
+        std::cout << "Enter a domain name: " << std::endl;
+
+        //Connect to server
+
+        //Create a DNS packet
+
+        //Send packet to server
+
+        //Wait for response
+
+        //Print response
+    }
+
+    //Close
+}
